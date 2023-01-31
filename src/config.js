49bcd2624f0752faf6fcdd983c90b8e5e4b691d6
Kapil Kandel <kandelkapil01@gmail.com>
2023-01-31 15:31:22 +0545

    api key updated

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
  const config = {
- 'ip_key' : 'at_ShqNiVwugm2BYBaAAxhAjywkoOrAV',
+ 'ip_key' : 'at_Rg90f8wOwU77ffBjKaHHmffBgJ4P3',
  'mapBox_token' :'pk.eyJ1Ijoia2FuZGVsa2FwaWwiLCJhIjoiY2t4cG80MGxzMDAzbzJ2c2J5a2Z1NHg2aSJ9.GU9WtDpJ9dDGn1JNySDO2A'
 };
