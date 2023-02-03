f42ad752cbd13eb803ca2132381d55704939f19d
Kapil Kandel <kandelkapil01@gmail.com>
2023-02-03 17:25:01 +0545

    key updated

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
  const config = {
- 'ip_key' : 'at_ShqNiVwugm2BYBaAAxhAjywkoOrAV',
+ 'ip_key' : 'at_goZX6CJBgdRcYb52SiwVpvafxgKsF',
  'mapBox_token' :'pk.eyJ1Ijoia2FuZGVsa2FwaWwiLCJhIjoiY2t4cG80MGxzMDAzbzJ2c2J5a2Z1NHg2aSJ9.GU9WtDpJ9dDGn1JNySDO2A'
 };
