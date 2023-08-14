91f84c90b90003f3dc0720a98335019902717910
Kapil Kandel <kapil@evolveasia.co>
2023-08-14 22:04:58 +0545

    api key updated

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
- const config = {
- 'ip_key' : 'at_goZX6CJBgdRcYb52SiwVpvafxgKsF',
- 'mapBox_token' :'pk.eyJ1Ijoia2FuZGVsa2FwaWwiLCJhIjoiY2t4cG80MGxzMDAzbzJ2c2J5a2Z1NHg2aSJ9.GU9WtDpJ9dDGn1JNySDO2A'
+const config = {
+  ip_key: "at_x7zZxu6lNYemNWXjpmDXFa5KZb496",
+  mapBox_token:
+    "pk.eyJ1Ijoia2FuZGVsa2FwaWwiLCJhIjoiY2t4cG80MGxzMDAzbzJ2c2J5a2Z1NHg2aSJ9.GU9WtDpJ9dDGn1JNySDO2A",
 };
